a92e37b216fae8677e3824e8a0004d6800f76f4b
Krivtsov Roman <raxpost@gmail.com>
2015-03-13 13:52:59 +0300

    js changes

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 var async = require("async");
 var http = require("http");
 var net = require("net");
+var fs = require("fs");
 var cluster = require('cluster');
 
 var options = {
-  host: 'www.google.com',
+  host: '127.0.0.1',
   port: 80,
   path: '/',
   method: 'GET'
@@ -12,14 +13,15 @@ var options = {
 
 
 var rps = 400;
-var numCPUs = 4;
-var isFork = true; // do fork
+var numCPUs = 50;
+var isFork = false; // do fork
 
 var row=[];
 for (var i = 0; i<rps; i++) {row.push(i);}
 var n =0;
 var resps = [];
 var total=0;
+var maxRPS = 0;
 
 // HTTP wrapper
 
@@ -47,7 +49,7 @@ var executer_ = function (i, next) {
 var executer = function (i, next) {
     var body = '';
     var ts = Date.now();
-    var client = net.connect(80, "www.google.com", function () {
+    var client = net.connect(80, "google.com", function () {
         client.write('GET / HTTP/1.1\r\n\r\n');
         client.end();
     });
@@ -79,12 +81,13 @@ if (isFork && cluster.isMaster) {
     }, function () {
         console.log(resps.length, resps[resps.length-1]);
         console.log("Total", total, Date.now() - totalTs); // total done requests and eaten time
+        fs.appendFileSync("/tmp/res.txt", maxRPS + "\n");
 
     });
 
 
     setInterval(function () { // RPS reports
-
+        if (n>maxRPS) maxRPS=n;
         console.log("RPS:", n);
         n=0;
 
